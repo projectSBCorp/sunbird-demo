16ad8aa2ab7ce9a7edd1e73497c1d682ea70c951
Kamaleeswary <kamaleeanbu@gmail.com>
2018-12-03 13:46:10 +0530

    Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { CardComponent } from './card/card.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeroComponent,
+    CardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+//   $("#menu-toggle").click(function(e) {
+//     e.preventDefault();
+//     $("#wrapper").toggleClass("active");
+// });
+}
